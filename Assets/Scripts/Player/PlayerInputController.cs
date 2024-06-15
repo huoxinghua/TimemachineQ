91c481dac2e5aca4d4b5608597996e228f86332c
huoxinghua <huoxinghuacn@gmail.com>
2024-06-15 15:12:27 -0600

    two player can move and jump successfully;one is AD and space another is arrow

--- Assets/Scripts/Player/PlayerInputController.cs
+++ Assets/Scripts/Player/PlayerInputController.cs
@@ -6,7 +6,9 @@ using UnityEngine.InputSystem;
 public class PlayerInputController : MonoBehaviour
 {
     #region Private Variables
-    PlayerController playerController;
+    private PlayerController playerController;
+    public enum PlayerType {RedPlayer,BluePlayer}
+    [SerializeField] private PlayerType playerType;
     #endregion
 
 
@@ -14,18 +16,38 @@ public class PlayerInputController : MonoBehaviour
     {
         playerController = GetComponent<PlayerController>();
     }
+    
     void OnEnable()
     {
+        Debug.Log("get playerType" + playerType);
         PlayerInput playerInput = new PlayerInput();
+       
         if (playerInput != null)
         {
-            Debug.Log("get playerInput" + playerInput);
+            if (playerType == PlayerType.RedPlayer)
+            {
+
             playerInput.RedPlayerMovement.Move.performed += (val) => playerController.Move(val.ReadValue<float>());
             playerInput.RedPlayerMovement.Jump.performed += (Val) => playerController.Jump();
             playerInput.RedPlayerMovement.Jump.canceled += (val) => playerController.JumpReleased();
-        }
-        playerInput.Enable();
+            }
+            else if (playerType == PlayerType.BluePlayer) 
+            {
+                playerInput.BluePlayerMovement.Move.performed += (val) => playerController.Move(val.ReadValue<float>());
+                playerInput.BluePlayerMovement.Jump.performed += (Val) => playerController.Jump();
+                playerInput.BluePlayerMovement.Jump.canceled += (val) => playerController.JumpReleased();
+            }
 
+        }
+        if (playerType == PlayerType.RedPlayer)
+        {
+
+            playerInput.RedPlayerMovement.Enable();
+        }
+        else if (playerType == PlayerType.BluePlayer)
+        {
+            playerInput.BluePlayerMovement.Enable();
+        }
     }
     void Start()
     {
