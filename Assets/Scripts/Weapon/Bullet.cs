d29ae307b17e8f9c09958bed9c9366ac38c62cbf
Nandini <nandiniparmar7781@gmail.com>
2024-07-02 02:24:49 +0530

    Cant Despawn so added destroy for now

--- Assets/Scripts/Weapon/Bullet.cs
+++ Assets/Scripts/Weapon/Bullet.cs
@@ -1,15 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.EditorTools;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Bullet : PoolObject
-{
-    public float speed;
-    private Rigidbody2D rb;
-
-    private void Start()
+{   
+    private void OnCollisionEnter2D(Collision2D other)
     {
-        rb = GetComponent<Rigidbody2D>();
-        rb.velocity = transform.right * speed;
+        
+        Destroy(gameObject);
     }
 }
