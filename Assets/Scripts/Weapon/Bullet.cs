0599857efc73195ebf26c13b821b99f8ea40486e
Nandini <nandiniparmar7781@gmail.com>
2024-07-02 13:31:03 -0600

    fix the bullet direction and code

--- Assets/Scripts/Weapon/Bullet.cs
+++ Assets/Scripts/Weapon/Bullet.cs
@@ -2,13 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.EditorTools;
 using UnityEngine;
+using UnityEngine.Pool;
 using UnityEngine.UIElements;
 
 public class Bullet : PoolObject
-{   
+{
+    [SerializeField] private PoolManager poolmanager;
+
+    private void Start()
+    {
+        poolmanager = GetComponent<PoolManager>();
+    }
     private void OnCollisionEnter2D(Collision2D other)
     {
-        
-        Destroy(gameObject);
+        poolmanager.DeSpawn(this);
     }
 }
