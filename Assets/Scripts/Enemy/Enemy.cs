5749fdcba29c12cc6de66c40733171eb0cff1e84
Nandini <nandiniparmar7781@gmail.com>
2024-07-19 20:01:05 -0600

    Basic Enemy Script

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,10 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Enemy : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] private int _startPosition = 0;
+    [SerializeField] private int _endPosition = 1;
+    private float _minTargetDistance = 0.01f;
+    private PlayerController playerController;
+    private Rigidbody2D rb;
+    private GameManager gameManager;
+
+
     void Start()
     {
         
@@ -12,7 +22,41 @@ public class Enemy : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    IEnumerator MoveEnemy()
     {
+        float alpha = 0.0f;
+        while (true)
+        {
+            alpha += Time.fixedDeltaTime * moveSpeed;
+            if ((1.0f - alpha) < _minTargetDistance)
+            {
+                _startPosition++;
+                _endPosition++;
+                _startPosition %= movePoints.Length;
+                _endPosition %= movePoints.Length;
+                alpha = 0f;
+            }
+
+
+            this.transform.position = Vector3.Lerp(
+                this.movePoints[_startPosition].transform.position,
+                this.movePoints[_endPosition].transform.position, alpha
+                    );
+            yield return new WaitForFixedUpdate();
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        
+        if(other.GetComponent<PlayerController>())
+        {
+            Debug.Log("Enemy attack");
+            //GameManager.Instance.PlayerDied();
+            gameObject.SetActive(false);
+        }
+        
         
     }
 }
